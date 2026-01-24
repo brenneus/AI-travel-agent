14d7856d0bf401564494d1c86a678e84b5604c0e
Brennan Giglio <brennangiglio@gmail.com>
2026-01-23 21:17:56 -0500

    added first config.py file

---
+++ src/config.py
@@ -0,0 +1,17 @@
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+class Config:
+    # 1. Browser Settings
+    # True - brower runs in background; False - browser window is visible
+    HEADLESS = False 
+    TIMEOUT = 30000 
+    
+    # 2. The Human Mask
+    # Makes the browser appear more like a real user
+    USER_AGENT = "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/128.0.0.0 Safari/537.36"
+    
+    # 3. Model Name
+    MODEL_NAME = "claude-3-5-sonnet-20240620"
